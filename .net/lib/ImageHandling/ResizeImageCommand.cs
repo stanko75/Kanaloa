0da746844bb92656f84270f9cb44daf3e03453ae
Stanko Milošev <stanko75@users.noreply.github.com>
2024-06-12 20:33:44 +0200

    refactoring for clarity and better separation of concerns

--- .net/lib/ImageHandling/ResizeImageCommand.cs
+++ .net/lib/ImageHandling/ResizeImageCommand.cs
@@ -1,23 +1,33 @@
 ﻿using System.Data;
+using System.IO;
 
 namespace ImageHandling;
 
 public class ResizeImageCommand
 {
-    private string? _imageFileName = string.Empty;
-    private string? _imageOriginalFolderName = string.Empty;
+    private string? _folderName;
+    private string? _imageFileName;
+    private string? _imageOriginalFolderName;
+    private string? _imageThumbsFolderName;
 
     public string? OriginalFileName
     {
         get => Path.Join(_imageOriginalFolderName, _imageFileName);
-        set => _imageFileName = value;
+        set
+        {
+            _imageFileName = value;
+            UpdateFolderPaths();
+        }
     }
 
-    string? _imageThumbsFolderName = string.Empty;
     public string? SaveTo
     {
         get => Path.Join(_imageThumbsFolderName, _imageFileName);
-        set => _imageFileName = value;
+        set
+        {
+            _imageFileName = value;
+            UpdateFolderPaths();
+        }
     }
 
     public int CanvasWidth { get; set; }
@@ -25,13 +35,29 @@ public class ResizeImageCommand
 
     public void CreateDirectories(string folderName)
     {
-        _imageOriginalFolderName = Path.Join(folderName, "pics");
-        _imageThumbsFolderName = Path.Join(folderName, "thumbs");
+        if (string.IsNullOrWhiteSpace(folderName))
+            throw new NoNullAllowedException("folderName is null or empty!");
 
-        if (string.IsNullOrWhiteSpace(_imageOriginalFolderName)) throw new NoNullAllowedException("_imageOriginalFolderName is null!");
-        if (string.IsNullOrWhiteSpace(_imageThumbsFolderName)) throw new NoNullAllowedException("_imageThumbsFolderName is null!");
-        Directory.CreateDirectory(folderName);
+        _folderName = folderName;
+        UpdateFolderPaths();
+
+        if (string.IsNullOrWhiteSpace(_imageOriginalFolderName))
+            throw new NoNullAllowedException("_imageOriginalFolderName is null!");
+
+        if (string.IsNullOrWhiteSpace(_imageThumbsFolderName))
+            throw new NoNullAllowedException("_imageThumbsFolderName is null!");
+
+        Directory.CreateDirectory(_folderName);
         Directory.CreateDirectory(_imageOriginalFolderName);
         Directory.CreateDirectory(_imageThumbsFolderName);
+    }
+
+    private void UpdateFolderPaths()
+    {
+        if (!string.IsNullOrWhiteSpace(_folderName))
+        {
+            _imageOriginalFolderName = Path.Join(_folderName, "pics");
+            _imageThumbsFolderName = Path.Join(_folderName, "thumbs");
+        }
     }
 }
