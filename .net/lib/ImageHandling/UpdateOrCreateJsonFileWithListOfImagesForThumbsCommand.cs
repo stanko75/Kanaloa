e77175534efaaede9f58cfac8d5c0cc3b664e54e
Stanko Milošev <stanko75@users.noreply.github.com>
2024-06-14 20:56:20 +0200

    deleted commands which I actually dont need

--- .net/lib/ImageHandling/UpdateOrCreateJsonFileWithListOfImagesForThumbsCommand.cs
+++ .net/lib/ImageHandling/UpdateOrCreateJsonFileWithListOfImagesForThumbsCommand.cs
@@ -4,7 +4,5 @@ public class UpdateOrCreateJsonFileWithListOfImagesForThumbsCommand
 {
     public string? KmlFileName { get; set;}
     public string? FolderName { get; set; }
-    public string? RootUrl { get; set; }
-    public string? ImageFileName { get; set; }
     public LatLngModel? LatLngModel { get; set; }
 }
