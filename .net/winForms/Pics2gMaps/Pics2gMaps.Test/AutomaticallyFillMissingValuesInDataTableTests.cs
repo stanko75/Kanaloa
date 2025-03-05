540e245178cc43f37b7105f12ca7b3effdbfec19
Stanko Milosev <s.milosev@stotax.de>
2025-03-05 08:43:22 +0100

    added test

---
+++ .net/winForms/Pics2gMaps/Pics2gMaps.Test/AutomaticallyFillMissingValuesInDataTableTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Pics2gMaps.Test
+{
+    [TestClass]
+    public sealed class AutomaticallyFillMissingValuesInDataTableTests
+    {
+        [TestMethod]
+        public void AutomaticallyFillMissingValuesInDataTableCheckIfOk()
+        {
+            AutomaticallyFillMissingValuesInDataTableCommand automaticallyFillMissingValuesCommand =
+                new AutomaticallyFillMissingValuesInDataTableCommand
+                {
+                    DataRow = dataRow,
+                    Columns = _dtGalleryConfiguration.Columns,
+                    BaseUrl = BaseUrl,
+                    JqueryVersion = JqueryVersion
+                };
+        }
+    }
+}
