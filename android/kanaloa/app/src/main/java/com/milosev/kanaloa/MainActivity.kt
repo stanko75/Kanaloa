36918f063d7302db1141f99803fed2bbecce3aa0
Stanko Milošev <stanko75@users.noreply.github.com>
2025-03-27 21:02:05 +0100

    open empty SettingsActivity

--- android/kanaloa/app/src/main/java/com/milosev/kanaloa/MainActivity.kt
+++ android/kanaloa/app/src/main/java/com/milosev/kanaloa/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.milosev.kanaloa
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
+import android.view.MenuItem
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -13,6 +14,7 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.milosev.kanaloa.databinding.ActivityMainBinding
 import com.milosev.kanaloa.location.LocationPermissionHelper
+import com.milosev.kanaloa.ui.settings.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -47,6 +49,19 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                // Launch settings activity here
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
