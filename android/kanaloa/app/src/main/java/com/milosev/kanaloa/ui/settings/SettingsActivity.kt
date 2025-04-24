f260269d0bf73af46471cc923ea64cc96b3238b8
Stanko Milošev <stanko75@users.noreply.github.com>
2025-04-24 20:12:58 +0200

    save settings

--- android/kanaloa/app/src/main/java/com/milosev/kanaloa/ui/settings/SettingsActivity.kt
+++ android/kanaloa/app/src/main/java/com/milosev/kanaloa/ui/settings/SettingsActivity.kt
@@ -1,13 +1,19 @@
 package com.milosev.kanaloa.ui.settings
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.milosev.kanaloa.R
+import com.milosev.kanaloa.databinding.ActivitySettingsBinding
+import androidx.core.content.edit
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +22,28 @@ class SettingsActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val sharedPreferences =
+            getSharedPreferences("ftpSettings", Context.MODE_PRIVATE)
+
+        val host = sharedPreferences.getString("host", "ftp.host.com")
+        val user = sharedPreferences.getString("user", "user")
+        val pass = sharedPreferences.getString("pass", "pass")
+
+        binding.editTextHost.setText(host)
+        binding.editTextUser.setText(user)
+        binding.editTextPass.setText(pass)
+
+        binding.btnSaveSettings.setOnClickListener {
+            sharedPreferences.edit() {
+                putString("host", binding.editTextHost.text.toString())
+                putString("user", binding.editTextUser.text.toString())
+                putString("pass", binding.editTextPass.text.toString())
+            }
+            finish()
         }
     }
 }
