d4f299652e2f46a31cec785595525f86db155191
Stanko Milošev <stanko75@users.noreply.github.com>
2025-06-28 21:12:15 +0200

    android upgrade

--- android/kanaloa/app/build.gradle.kts
+++ android/kanaloa/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.milosev.kanaloa"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.milosev.kanaloa"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
