cc5b6810fa23595f028eada6f2d11a3a17166387
Stanko Milošev <stanko75@users.noreply.github.com>
2026-07-05 18:32:25 +0200

    read and set from sharedPreferences

--- android/kanaloa/app/src/main/java/com/milosev/kanaloa/ui/settings/PhpUploadSettingsActivity.kt
+++ android/kanaloa/app/src/main/java/com/milosev/kanaloa/ui/settings/PhpUploadSettingsActivity.kt
@@ -3,8 +3,10 @@ package com.milosev.kanaloa.ui.settings
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.milosev.kanaloa.SharedPreferencesGlobal
 import com.milosev.kanaloa.databinding.ActivityPhpUploadSettingsBinding
 
 class PhpUploadSettingsActivity : AppCompatActivity() {
@@ -21,6 +23,14 @@ class PhpUploadSettingsActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
 
+        val sharedPreferences = getSharedPreferences(SharedPreferencesGlobal.JoomlaSettings, MODE_PRIVATE)
+        binding.etPhpUploadUrl.setText(sharedPreferences.getString("phpUploadUrl", ""))
+
+        binding.btnSave.setOnClickListener {
+            sharedPreferences.edit {
+                putString("phpUploadUrl", binding.etPhpUploadUrl.text.toString())
+            }
+        }
+    }
 }
